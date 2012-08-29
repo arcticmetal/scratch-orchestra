5748c245c75b112c2ff9ec795aea69ef24ea6bd9
Breilly38 <brendanreilly38@gmail.com>
2012-08-29 17:41:34 -0400

    initial commit

---
+++ src/newpythonproject.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Brendan"
+__date__ ="$Aug 29, 2012 5:33:46 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
